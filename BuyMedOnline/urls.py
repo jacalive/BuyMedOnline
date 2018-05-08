07dd4370f0c832eeae30c78e9cafc2830c729901
shuyin0321 <1506968772@qq.com>
2018-05-08 23:18:29 +0800

    update

--- BuyMedOnline/urls.py
+++ BuyMedOnline/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from users import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('users/',include('django.contrib.auth.urls')),
-    path(r'^$',views.index,name='index'),
+    path('index/',include('users.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='users'
 urlpatterns=[
 	path('register/',views.register,name='register'),
+	path(r'^$',views.index,name='index'),
 ]
