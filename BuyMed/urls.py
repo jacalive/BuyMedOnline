b7dfa22b77529b76f6ac4fcab5398b345fc83538
qbing <songdy0x0tter@gmail.com>
2018-06-04 15:39:47 +0800

    update cart,buymed

--- BuyMed/urls.py
+++ BuyMed/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path, re_path
 from . import views
+from django.contrib.auth.views import login, logout
 
 app_name = 'BuyMed'
 urlpatterns=[
-	re_path(r'^$', views.product_list, name='product_list'),
-    re_path(r'^(?P<category_slug>[-\w]+)/$',
-            views.product_list,
-            name='product_list_by_category'),
-    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
-            views.product_detail,
-            name='product_detail'),
+	# re_path(r'^$', views.product_list, name='product_list'),
+        re_path(r'^product/$', views.product_list, name='product_list'),
+        re_path(r'^(?P<category_slug>[-\w]+)/$',
+                views.product_list,
+                name='product_list_by_category'),
+        re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+                views.product_detail,
+                name='product_detail'),
+        re_path(r'^product/search/$', 
+                views.product_name_search,
+                name='product_name_search'),
+        re_path(r'^product/search/([a-zA-Z]+)/$',
+                views.product_name_search,
+                name="product_name_search"),
+        re_path(r'^product/login/$', login),
+        re_path(r'^product/logout/$', logout),
 ]
--- BuyMed/views.py
+++ BuyMed/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, get_list_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import Product,Category
 from cart.forms import CartAddProductForm
+from itertools import chain
 
 # Create your views here.
 def product_list(request,category_slug=None):
@@ -17,7 +18,7 @@ def product_list(request,category_slug=None):
 				'categories': categories,
 				'products': products})
 
-def product_detail(request,id,slug):
+def product_detail(request, id, slug):
 	product=get_object_or_404(Product,id=id,slug=slug,available=True)
 	cart_product_form = CartAddProductForm()
 	print(product.price)
@@ -25,3 +26,21 @@ def product_detail(request,id,slug):
 				'BuyMed/product/detail.html',
 				{'product':product,
 				'cart_product_form': cart_product_form})
+
+def product_name_search(request, search_thing=None):
+	if search_thing == None:
+		search_thing = request.GET.get('search_thing')
+	error_message = ''		
+	print(search_thing)
+	# search_condition = {'name__icontains': search_thing, 'description__icontains': search_thing}
+	# products = Product.objects.filter(**search_condition)
+	p_name = Product.objects.filter(name__iexact=search_thing)
+	p_desc = Product.objects.filter(description__iexact=search_thing)
+	
+	products = chain(p_name, p_desc)
+	return render(request,
+			'BuyMed/product/list.html',
+			{'products': products})
+
+def back_to_login(request):
+	return render(request, 'registration/login.html')
